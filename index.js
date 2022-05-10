511bcbdd4533fe100af12ee9b2a2c6c2d419406b
Lautaro <99227381+speedbuild98@users.noreply.github.com>
2022-05-10 09:36:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const displayValorActual = document.getElementById('valor-actual');
 const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador');
 
-const display = new.Display(displayValorAnterior, displayValorActual);
+const display = new Display(displayValorAnterior, displayValorActual);
 
-botonesNumeros.forEach(boton =>{
-      boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
-      });
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
 
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value))
+});
       
